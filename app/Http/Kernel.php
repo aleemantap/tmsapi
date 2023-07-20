d010d05cc46e096c33484ddf6cab4ff9e807d45c
aleemantap <aleemantap@gmail.com>
2023-07-21 02:58:46 +0700

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'HeaderAccess' => \App\Http\Middleware\HeaderAccess::class,
     ];
 }
