45398776d54abd1d8c8d91de5d087a85ac2dca19
aleemantap <aleemantap@gmail.com>
2023-07-20 20:51:34 +0700

    all

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/file',[FileController::class, 'store']);
+Route::get('/file',[FileController::class, 'show']);
+//Route::post('/file', 'FileController@store');
+//Route::post('/files', 'FilesController@simpan');
+//Route::get('/file', 'FileController@show');
+//Route::get('/files', 'FilesController@show');
+Route::get('/cek', function (Request $request) {
+   return "hello";
+});
